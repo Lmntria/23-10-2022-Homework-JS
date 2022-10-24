88b74462e5a7e58ddf37ca7a388f29af0057896e
Lmntria <seymur1095@gmail.com>
2022-10-24 22:10:37 +0400

    redacte p-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ let info=document.getElementById("correction")
 let namearr=[];
 let surarr=[]
 let count=2
+let surname=submit.previousElementSibling;  
+let name=surname.previousElementSibling
 submit.addEventListener('click',function() {
-  let surname=this.previousElementSibling;  
-  let name=surname.previousElementSibling
-  if ((surname.value.trim()>0 && name.value.trim()>0) || (name.value.length>2 && surname.value.length>5 )) {
+  if ((surname.value.trim()>0 && name.value.trim()>0) || (name.value.length>2 && surname.value.length>4 )) {
     count++
     let tableBody=document.getElementById("table-body")
     tableBody.innerHTML+=
@@ -15,14 +15,13 @@ submit.addEventListener('click',function() {
       <td>${name.value}</td>
       <td>${surname.value}</td> 
       <td class="text-center">
-        <button onclick="this.parentElement.parentElement.parentElement.classList.toggle('active-ls')" class="btn btn-success done-btn col-md-5">Redacte</button>
+        <button onclick="redacte(this)" class="btn btn-success done-btn col-md-5">Redacte</button>
         <button onclick="this.parentElement.parentElement.remove()" class="btn btn-danger col-md-5">Delete</button>
       </td>
     </tr>
     `
     surname.value=""
     name.value=""
-    console.log(name)
     info.style="display: none !important"
     surname.style.borderColor="#ced4da"
     name.style.borderColor="#ced4da"
@@ -34,3 +33,10 @@ submit.addEventListener('click',function() {
     info.style="display: block !important"
   }
 })
+
+function redacte(x) {
+  let inputsurname=x.parentElement.previousElementSibling.innerText
+  let inputname=x.parentElement.previousElementSibling.previousElementSibling.innerText
+  name.value=inputname
+  surname.value=inputsurname
+}
